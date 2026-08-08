088fbcada1c9673b566dd069f01fc0ce99b6e16b
Pretha P <palanisamypretha@gmail.com>
2026-08-08 19:31:14 +0530

    settings

--- core/views.py
+++ core/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Crime
 from .forms import CrimeForm
 from django.db.models import Q  # idha top la import pannu
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
-from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate,login
 from django.contrib.auth import views as auth_views
+
+def my_login(request):  # <- idhu illama dhan error
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'core/login.html')
 
 @login_required
 def home(request):
