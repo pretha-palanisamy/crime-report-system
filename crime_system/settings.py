259ef473c18eb26c722a278a20ec1f31a4b1214d
Pretha P <palanisamypretha@gmail.com>
2026-08-08 09:28:02 +0530

    Fix ALLOWED_HOSTS for Render

--- crime_system/settings.py
+++ crime_system/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+DEBUG=os.environ.get('DEBUG','False')=='True'
+SECRET_KEY=os.environ.get('SECRET_KEY','django-insecure-default-key')
 
+ALLOWED_HOSTS=os.environ.get('ALLOWED_HOSTS','*').split(',')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
