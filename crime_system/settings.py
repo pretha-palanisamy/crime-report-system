b70a06d9ccdf8d942de37daefb56901a4248c651
Pretha P <palanisamypretha@gmail.com>
2026-08-08 13:07:09 +0530

    core

--- crime_system/settings.py
+++ crime_system/settings.py
@@ -60,7 +60,7 @@ BASE_DIR=Path(__file__).resolve().parent.parent
 TEMPLATES = [
     {   
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR/'core'/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- crime_system/urls.py
+++ crime_system/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='core/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(next_page='login'),name='logout'),
     path('', views.home), # root url
+    path('',include('crime_app.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
